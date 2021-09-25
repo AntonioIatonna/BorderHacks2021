ac215dccd249cbe0db7ed2a7bb20bdcbcf1c03f1
Ali <85372689+klkch@users.noreply.github.com>
2021-09-25 11:58:04 -0400

    Added JQuery

--- scripts/map.js
+++ scripts/map.js
@@ -1,5 +1,11 @@
 let map;
 
+$.getJSON("../data/snowplow-2021-03-22_2021-03-28.json", function(json) {
+    var data = json
+    console.log(data);
+});
+
+
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 42.29086877112532, lng: -83.00613243683966 },
