99665ccb2c76b8904855fa9df7b5910df1267ba7
Gabriel <gabriel.i.rueda@gmail.com>
2021-09-25 14:26:08 -0400

    Should actually work

--- scripts/map.js
+++ scripts/map.js
@@ -1,13 +1,23 @@
 let map;
 
-async function getData() {
-  const response = await fetch('../data/snowplow-2021-03-22_2021-03-28.json');
-  const data = await response.json();
-  console.log(data[0])
-  // logs [{ name: 'Joker'}, { name: 'Batman' }]
-}
 
-getData();
+  const address = fetch("../data/snowplow-2021-03-22_2021-03-28.json")
+  .then((response) => response.json())
+  .then((data) => {
+    return data;
+  });
+
+const plotPoint = async (id) => {
+  const a = await address;
+  //write code to plot the point here...
+  var coordinates = [a[id].Latitude, a[id].Longitude];
+  console.log(coordinates);
+};
+
+plotPoint(0);
+
+
+
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
